55687a60b17816ca5f05af2b8dea4b7edb7b8cad
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-05-26 10:05:28 -0300

    Add files via upload

---
+++ Exercicio_1.cs
@@ -0,0 +1,14 @@
+/******************************************************************************
+1) Comprei 500 balas comi 305 e dei 120 para meu irmão. Quantas eu fiquei? 
+*******************************************************************************/
+using System;
+class HelloWorld {
+  static void Main() {
+      
+        int balas = 500;
+		int comi = 305;
+		int dei = 120;
+      
+    Console.WriteLine("Fique com: " + (balas-comi-dei)+ " balas.");
+  }
+}
