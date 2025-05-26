b23d562c126b55f8b40ab1a1e87a6bcc5e8d4169
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-05-26 10:01:40 -0300

    Add files via upload

---
+++ Exercicio_1.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+1) Comprei 500 balas comi 305 e dei 120 para meu irmão. Quantas eu fiquei? 
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		int balas = 500;
+		int comi = 305;
+		int dei = 120;
+		
+		System.out.print ("Fique com: " + (balas-comi-dei)+ " balas.");
+	}
+}
