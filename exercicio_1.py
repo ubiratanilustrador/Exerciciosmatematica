14603428ba5edd1600ad4270aa12ed575b1fcc08
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-05-28 11:39:09 -0300

    Create exercicio_1.py

---
+++ exercicio_1.py
@@ -0,0 +1,11 @@
+'''
+1) Comprei 500 balas comi 305 e dei 120 para meu irmão. Quantas eu fiquei?
+'''
+def balas():
+    comprei = 500
+    comi = 305
+    dei = 120
+    fiquei = comprei - comi - dei
+    print('fiquei com: ',fiquei,' balas.')
+    
+balas()
