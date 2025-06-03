0ad300a2abcdbafc15b57b77cd3b102e737cf43a
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-06-03 17:14:45 -0300

    Create Exercicio_4.py

---
+++ Exercicio_4.py
@@ -0,0 +1,20 @@
+'''
+4) Em um projeto para a construção de um cinema, os arquitetos estão avaliando a 
+   relação entre a quantidade de fileiras e a quantidade de cadeiras em cada fileira. 
+   O projeto inicial prevê uma sala para 304 pessoas. No caso de utilizarem 19 fileiras,
+   o número de cadeiras por fileira será
+   a) 14.
+   b) 15.
+   c) 16Q.
+   d) 13.
+   e) 12.
+'''
+def operacao ():
+    
+    capacidade = 304
+    fileiras = 19
+    resultado = capacidade / fileiras;
+    print("Serão necessários: ",resultado," cadeiras.")
+  
+operacao()
+    
