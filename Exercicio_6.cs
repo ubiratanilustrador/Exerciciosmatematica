248cf558b67ab7a322aafc6ceebda16d35e4f556
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-06-15 16:15:33 -0300

    Create Exercicio_6.cs

---
+++ Exercicio_6.cs
@@ -0,0 +1,68 @@
+/******************************************************************************
+   Equação do segundo Grau  é uma equação polinomial onde o termo de maior 
+   grau da variável é o quadrado. A sua forma geral é ax² + bx + c = 0, 
+   onde 'a', 'b' e 'c' são coeficientes (com 'a' ≠ 0) e 'x' é a variável. 
+   Para resolver uma equação do 2º grau, utiliza-se a fórmula de Bhaskara 
+   (fórmula resolvente) para encontrar as raízes da equação. 
+
+*******************************************************************************/
+
+using System;
+class Exercicio_6 {
+  static void Main() {
+      
+    /* crio os coefiencentes da equação do segundo grau
+	 * e peço ao usuario que digite o valor dos três
+	 * coefientes*/
+    Console.WriteLine("informe o coeficente de a");
+    int a = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("informe o coeficente de b");
+    int b = Convert.ToInt32(Console.ReadLine()); 
+    Console.WriteLine("informe o coeficente de c");
+    int c = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine(" ");
+    
+    //exibi o resultado do delta no console
+    Console.WriteLine("Delta = "+ delta(a,b,c));
+    
+    /* crio uma varivel raizes que recebe o resultado de delta 
+	* e servira de parametro no calculo das duas raizes*/
+	double raizes = delta(a,b,c);
+	
+	//exibi o resultado as raizes no console
+	Console.WriteLine(" ");
+	Console.WriteLine("raiz 1 = "+ raize_um(a,b,raizes)+" raize 2 = "+ raize_dois(a,b,raizes));
+    
+  }
+  
+  //função responsavel por calcular o delta na equação segundo grau
+	private static double delta(int a,int b,int c) {
+	
+		//crio uma variavel que recebe o resultado de delta
+		double resultado = (Math.Pow(b, 2)) - (4*a*c);
+		
+		//retorno o resultado
+		return resultado;
+	}
+	
+	//função responsavel por calcular a primeira raiz
+	private static double raize_um(int a,int b,double raizes) {
+		 
+		//crio uma variavel que recebe o resultado da primeira raiz
+		double x1 = ((-b + Math.Sqrt(raizes))/2 *a);
+		
+		//retorno a primeira raiz
+		return x1;
+	}
+	
+	//função responsavel por calcular a segunda raiz
+	private static double raize_dois(int a,int b,double raizes) {
+		
+		//crio uma variavel que recebe o resultado da segunda raiz
+		double x2 = ((-b - Math.Sqrt(raizes))/2 *a);
+		
+		//retorno a segunda raiz
+		return x2;
+	}
+	
+}
