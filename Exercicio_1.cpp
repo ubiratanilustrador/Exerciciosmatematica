5be59d65d6c65b82dbd1b08e496144769a6ce373
Ubiratan_Ilustrador <94130386+ubiratanilustrador@users.noreply.github.com>
2025-07-26 23:24:28 -0300

    Create Exercicio_1.cpp

---
+++ Exercicio_1.cpp
@@ -0,0 +1,14 @@
+/******************************************************************************
+1) Comprei 500 balas comi 305 e dei 120 para meu irmão. Quantas eu fiquei?
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int balas = 500; // crio uma variavel e atribuo um valor a ela
+    int comi = 305; // crio uma variavel e atribuo um valor a ela
+    int dei = 120; // crio uma variavel e atribuo um valor a ela
+    int resultado = balas-comi-dei; // crio uma variavel que recebe a subtração de balas, comi e dei
+    std:: cout << resultado; // exibo no console o resultado 
+}
